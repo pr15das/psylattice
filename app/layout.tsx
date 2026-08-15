6a7182fb16920ccbb6d5b404cc6eb524f35bcbe9
Priyangshu Das <priyangshu152002net@gmail.com>
2026-08-16 02:33:35 +0530

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
-export const metadata = {
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
   metadataBase: new URL("https://psylattice.com"),
 
   title: {
@@ -10,4 +13,21 @@ export const metadata = {
 
   description:
     "PsyLattice is a platform for psychological assessment, monitoring, research and connected care.",
+
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-icon.png",
+  },
 };
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
